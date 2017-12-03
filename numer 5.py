cd71ce33facd4416f3d55086700ffd0c52840332
sunita <sunitakwr21@gmail.com>
2017-12-03 14:02:30 +0545

    "BIRTHDAY WISH"

---
+++ numer 5.py
@@ -0,0 +1,9 @@
+def f(name):
+    print("*" * 30)
+    print("             happy birthday  dear         ")
+    print(name)
+    print("*" * 30)
+
+    print("HAPPY BIRTHDAY {}".format(name))
+
+f('AAYUSHA')
